8fcd1e2fee90355f4b73d4a74932125088acd7cc
Gaetankevin <130754860+Gaetankevin@users.noreply.github.com>
2025-06-07 01:30:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+    base :'/ecommerce',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
